5c731af50e77aba863b8c5fbef6fc0857f7284a8
NYCOLASAUGUSTO <sad@asda>
2026-09-14 19:37:26 -0300

    Aula Crud

--- Exercicios De Aula/Aula Criar Listar e deletar/Aula8/app/src/main/java/com/example/aula8/MainActivity.kt
+++ Exercicios De Aula/Aula Criar Listar e deletar/Aula8/app/src/main/java/com/example/aula8/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.aula8.ui.theme.Aula8Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,4 +35,16 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+@Preview
+fun telaMercado(){
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Card(
+            modifier = Modifier.size(50.dp),
+            border = BorderStroke(2.dp, Color.DarkGray)
+        ) { }
 
+    }
+}
